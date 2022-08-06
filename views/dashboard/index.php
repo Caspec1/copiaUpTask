e3621e59357d27596dcf99f1f3a268aa5851ca4d
Javier Miranda <mirandavillegasjavier@gmail.com>
2022-08-06 09:56:19 -0400

    arreglo de routing

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,7 +1,7 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
     <?php if(count($proyectos) === 0) { ?>
-        <p class="no-proyectos">No hay proyectos aún <a href="/proyectos/crear-proyecto">Comienza Creando Uno</a></p>
+        <p class="no-proyectos">No hay proyectos aún <a href="/crear-proyecto">Comienza Creando Uno</a></p>
     <?php } else { ?>
         <ul class="listado-proyectos">
             <?php foreach ($proyectos as $proyecto) : ?>
